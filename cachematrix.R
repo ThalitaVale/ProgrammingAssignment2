210e56f2123c6c9ec7681a4f8f1e6f1c9aeac4ef
ThalitaVale <thalita.vale@gmail.com>
2018-08-10 17:23:05 -0300

    submit

--- cachematrix.R
+++ cachematrix.R
@@ -10,14 +10,18 @@
 makeCacheMatrix <- function(x = matrix()) {
 
   inv <- NULL
+  
   set <- function(y) {
     x <<- y
     inv <<- NULL
   }
   
   get <- function() x
+  
   setInv <- function(solve) inv <<- solve
+  
   getInv <- function() inv
+  
   list(set = set, get = get,
        setInv = setInv,
        getInv = getInv)
@@ -31,10 +35,12 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         
   inv <- x$getInv()
+  
   if(!is.null(inv)) {
     message("getting cached data")
     return(inv)
   }
+  
   mat <- x$get()
   inv <- solve(mat, ...)
   x$setInv(inv)
